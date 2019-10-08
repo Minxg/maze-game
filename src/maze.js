c7a55b00c4a7c9b937a5db3f6f3db12467903aeb
minxiaogen <minxiaogen@bilibili.com>
2019-10-08 09:27:44 +0800

    WIP: add a charactor

--- src/maze.js
+++ src/maze.js
@@ -3,16 +3,17 @@ var Maze = function(doc, elemId) {
   this.width = this.canvas.width;
   this.height = this.canvas.height;
   this.ctx = this.canvas.getContext('2d');
-  this.horizCells = 30;
-  this.vertCells = 30;
+  this.horizCells = 62;
+  this.vertCells = 62;
   this.generator = new MazeGenerator(this.horizCells, this.vertCells);
   this.cellWidth = this.width / this.horizCells;
   this.cellHeight = this.height / this.vertCells;
   
   var self = this;
-
+  var lineWidth = 1
   self.ctx.strokeStyle = "rgb(0, 0, 0)";
   self.ctx.fillStyle = "rgba(255, 0, 0, 0.1)";
+  self.ctx.lineWidth = lineWidth;
 
   return {
     width: function() {
@@ -30,6 +31,15 @@ var Maze = function(doc, elemId) {
     draw: function() {
       this.drawBorders();
       this.drawMaze();
+      this.drawCharactor();
+    },
+
+    drawCharactor: function() {
+      var charactor = new Image();
+      charactor.onload = function() {
+        self.ctx.drawImage(charactor, self.cellWidth * .2, self.cellHeight * .2, self.cellWidth * .6, self.cellHeight * .6);
+      }
+      charactor.src = './avatar.png'
     },
 
     solve: function() {
@@ -57,6 +67,36 @@ var Maze = function(doc, elemId) {
           }, 80 * i);
         })();
       }
+    },
+
+    drawMaze2: function() {
+      var graph = self.generator.graph;
+      var removedEdges = graph.removedEdges;
+      var width = graph.width;
+      var height = graph.height;
+      for(var i = 0; i < width - 1; i++) {
+        var x = self.cellWidth * (i + 1);
+        this.drawLine(x, 0, x, height * self.height);
+      }
+      for (var j = 0; j < height - 1; j++) {
+        var y = self.cellWidth * (j + 1);
+        this.drawLine(0, y, width * self.width, y);
+      }
+      removedEdges.forEach(function(edge) {
+        var cell1 = edge[0]
+        var cell2 = edge[1]
+        if (cell1.x === cell2.x) {
+          var y = Math.max(cell1.y, cell2.y) * self.cellHeight
+          // self.ctx.clearRect(cell1.x * self.cellWidth + lineWidth, y, (cell2.x + 1) * self.cellWidth - lineWidth, lineWidth)
+          this.clipLine(cell1.x * self.cellWidth + lineWidth, y, (cell2.x + 1) * self.cellWidth - lineWidth, y)
+        }
+        if (cell1.y === cell2.y) {
+          var x = Math.max(cell1.x, cell2.x) * self.cellHeight
+          // self.ctx.clearRect(x, cell1.y * self.cellHeight + lineWidth, lineWidth, (cell2.y + 1) * self.cellHeight - lineWidth)
+          this.clipLine(x, cell1.y * self.cellHeight + lineWidth, x, (cell2.y + 1) * self.cellHeight - lineWidth)
+        }
+        // var edgeLine = 
+      }, this)
     },
 
     drawMaze: function() {
@@ -116,6 +156,20 @@ var Maze = function(doc, elemId) {
           }          
         }
       }
+    },
+
+    clipLine: function (x1, y1, x2, y2) {
+      // self.ctx.lineCap = "round";　　//设置线条两端为圆弧
+      // self.ctx.lineJoin = "round";　　//设置线条转折为圆弧
+      self.ctx.lineWidth = 5;
+      // self.ctx.strokeStyle = "rgb(0, 0, 0, 1)";
+      self.ctx.globalCompositeOperation = "destination-out";
+      self.ctx.save()
+      self.ctx.beginPath();
+      self.ctx.moveTo(x1, y1);
+      self.ctx.lineTo(x2, y2);
+      self.ctx.stroke();
+      self.ctx.restore();
     },
 
     drawLine: function(x1, y1, x2, y2) {
